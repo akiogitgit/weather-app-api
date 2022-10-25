455df103a0443b9262e6256d3eeb22d648de34fa
moro1015 <moro1n0a1o5y0a1@yahoo.co.jp>
2022-10-25 17:08:17 +0900

    fastapi

---
+++ main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import json
+
+# ------------------------------------------------------------------
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+	rvalue = {"morning": "おはよう","afternoon": "こんにちは"}
+	return rvalue
+
+
+@app.get("/weather")
+async def weather():
+	current = Path()
+	file_path = current /  "weather.json"
+	
+	response = FileResponse(path=file_path) 
+	 
+	return response
+
+@app.get("/weather/city_names")
+async def city_names():
+
+# json read
+	current = Path()
+	file_path = current /  "weather.json"
+	with open(file_path, "r") as file:
+		dict = json.load(file)
+
+		result = []
+
+		for r in range(0, 8):
+			result.append(dict["data"][r]["name"])
+
+		return result
+
+
+@app.get("/weather/{city_name}")
+async def weather(city_name: str):
+
+# json read
+	current = Path()
+	file_path = current /  "weather.json"
+	with open(file_path, "r") as file:
+		dict = json.load(file)
+
+
+	response = dict["data"][0]
+	
+	for r in range(0, 8):
+	    if dict["data"][r]["name"].lower() == city_name.lower():
+		    return dict["data"][r]
+		
+
+
