315ff6b71de6959c98ab0c6e1b20c3699b8790cb
akiogitgit <akimaron2828@icloud.com>
2022-11-15 16:25:38 +0900

    feat: cors追加

--- app/main.py
+++ app/main.py
@@ -2,9 +2,18 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import json
+from starlette.middleware.cors import CORSMiddleware
 
 # ------------------------------------------------------------------
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 @app.get("/")
 def read_root():
